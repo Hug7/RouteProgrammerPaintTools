158f2aa36132f7d4a42f5dcf38961706a2e05b6f
liqi37 <m15044274156@163.com>
2020-04-06 22:45:32 +0800

    first commit

---
+++ RouteProgrammerPaintTools/urls.py
@@ -0,0 +1,36 @@
+"""RouteProgrammerPaintTools URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from paintApp import views
+
+urlpatterns = [
+    path('paintApp/', include('paintApp.urls')),
+    path('admin/', admin.site.urls),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # ex: /opt/?route=''&pickup=''&delivery=''&vehicle=''/
+    path('opt/', views.opt, name='opt')
+
+    # path('opt/<str:route>/<str:vehicle>/<str:pickup>/<str:delivery>', views.opt, name='opt')
+
+]
